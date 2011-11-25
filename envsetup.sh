172caddcb34f946a21b8296950c50cc7bc3af19c
Ricardo Cerqueira <github@cerqueira.org>
2011-11-25 15:30:36 +0000

    build: Preliminary support for ORM product builds
    If building a orm_* product, skip crawling the filesystem for
    AndroidProduct makefiles (and, of course, parsing their contents)
    and aim directly for the vendor/orm/devices/orm_<product> device configuration
    
    Change-Id: I2a5e70dda973a6fcdbba0d5e26b35b99d3f1aea2
    
    Change how we search for ORM makefiles.
    
    build: Fix fastpath code for ORM_BUILD
    
     * We need to adjust to the new validations
    
    Change-Id: I9066d4f437beb9597027ee6bbb52504b5e7e84c5
    Signed-off-by: Karan Srivastava <karan@onerom.org>

--- envsetup.sh
+++ envsetup.sh
@@ -68,6 +68,14 @@ function check_product()
         echo "Couldn't locate the top of the tree.  Try setting TOP." >&2
         return
     fi
+
+    if (echo -n $1 | grep -q -e "^orm_") ; then
+       ORM_BUILD=$(echo -n $1 | sed -e 's/^orm_//g')
+    else
+       ORM_BUILD=
+    fi
+    export ORM_BUILD
+
         TARGET_PRODUCT=$1 \
         TARGET_BUILD_VARIANT= \
         TARGET_BUILD_TYPE= \
