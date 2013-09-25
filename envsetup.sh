4f0f282578992a4d8c7150b2a696a51d7d1c8bc4
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-09-26 00:10:20 +0100

    Add a unique build number to every build
    SDK-readable via android.os.Build.INCREMENTAL
    
    Change-Id: If9885adebe12a56b2b2e353285721be695bc411b
    
    Fix exporting the unique build identifier
    
    Change-Id: Ie58e981b7e83a4351d137dfd8995dce7d365d950
    
    Fix incremental build number on Darwin
    
    Darwin does not include sha1sum, but it should have openssl.
    
    Change-Id: If2b685c1478f9486bed21dfba0c655d9e02db72b
    
    Use openssl instead of sha1sum
    
    Change-Id: Iab8a74d4af6646104cb23312853f811a38bdec0d
    
    Conflicts:
    	envsetup.sh

--- envsetup.sh
+++ envsetup.sh
@@ -71,6 +71,7 @@ function check_product()
 
     if (echo -n $1 | grep -q -e "^orm_") ; then
        ORM_BUILD=$(echo -n $1 | sed -e 's/^orm_//g')
+       export BUILD_NUMBER=$((date +%s%N ; echo $ORM_BUILD; hostname) | openssl sha1 | sed -e 's/.*=//g; s/ //g' | cut -c1-10)
     else
        ORM_BUILD=
     fi
