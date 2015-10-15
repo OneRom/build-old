dcdc66b113863113848c21fac7e890252ca82d52
JustArchi <JustArchi@JustArchi.net>
2015-10-15 16:16:10 +0200

    Build: Add support for specifying build variant in brunch/breakfast
    This little modification allows specifying build variant in brunch/
    breakfast commands. For example we can use "brunch shamu user" to build
    user variant instead of default userdebug.
    
    When no extra argument is given, userdebug is default.
    
    Signed-off-by: Karan Srivastava <karan@onerom.org>

--- envsetup.sh
+++ envsetup.sh
@@ -522,6 +522,7 @@ function brunch()
 function breakfast()
 {
     target=$1
+    local variant=$2
     ORM_DEVICES_ONLY="true"
     unset LUNCH_MENU_CHOICES
     add_lunch_combo full-eng
@@ -542,7 +543,10 @@ function breakfast()
             lunch $target
         else
             # This is probably just the CM model name
-            lunch orm_$target-userdebug
+            if [ -z "$variant" ]; then
+                variant="userdebug"
+            fi
+            lunch orm_$target-$variant
         fi
     fi
     return $?
