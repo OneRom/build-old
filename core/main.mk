6f3bc3647c3ad492fd1b26849623fe90603ad09f
Karan Srivastava <theofficiallysonyrebel@gmail.com>
2015-10-24 23:06:18 +0530

    Build : Make Qcom File Optional for mm6.0 tree

--- core/main.mk
+++ core/main.mk
@@ -103,7 +103,7 @@ include $(BUILD_SYSTEM)/config.mk
 include $(BUILD_SYSTEM)/cleanbuild.mk
 
 # Bring in Qualcomm helper macros
-include $(BUILD_SYSTEM)/qcom_utils.mk
+-include $(BUILD_SYSTEM)/qcom_utils.mk
 
 # Include the google-specific config
 -include vendor/google/build/config.mk
